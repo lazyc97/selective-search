f8933b270b2874f51a2baeb18decbd365007ddcf
Duc Hoang Nguyen <lazycoder97@gmail.com>
2017-11-18 17:41:21 +0700

    fix for new scikit-image version

--- hist.py
+++ hist.py
@@ -11,7 +11,7 @@ import scipy.ndimage.filters
 from sklearn.preprocessing import normalize
 
 def get_color_hist(img,segment_mask,n_bins=25):
-	''' 
+	'''
 	Computes color histograms for all the blobs
 	parameters
 	----------
@@ -22,7 +22,7 @@ def get_color_hist(img,segment_mask,n_bins=25):
 
 	returns
 	-------
-	
+
 	hist : color_histogram of the blobs. Shape: [ n_segments , n_bins*n_color_channels ]
 	'''
 	if img.max()>1:	_img = img/255.0
@@ -33,11 +33,11 @@ def get_color_hist(img,segment_mask,n_bins=25):
 	bins = [labels, bins]
 	hist = np.hstack([ np.histogram2d(segment_mask.flatten(), _img[:, :, i].flatten(), bins=bins)[0] for i in range(img.shape[-1]) ])
 	hist = normalize(hist,norm='l1',axis=1)
-	
+
 	return hist
 
 def get_texture_hist(img,segment_mask,n_orientation = 8, n_bins = 10):
-	''' 
+	'''
 	Computes texture histograms for all the blobs
 	parameters
 	----------
@@ -48,10 +48,10 @@ def get_texture_hist(img,segment_mask,n_orientation = 8, n_bins = 10):
 
 	returns
 	-------
-	
+
 	hist : texture histogram of the blobs. Shape: [ n_segments , n_bins*n_orientations*n_color_channels ]
 	'''
-	filt_img = skimage.filters.gaussian_filter(img, sigma = 1.0, multichannel = True).astype(np.float32)
+	filt_img = skimage.filters.gaussian(img, sigma = 1.0, multichannel = True).astype(np.float32)
 	op = np.array([[-1.0, 0.0, 1.0]])
 	grad_x = np.array([scipy.ndimage.filters.convolve(filt_img[:,:,i], op) for i in range(img.shape[-1])])
 	grad_y = np.array([scipy.ndimage.filters.convolve(filt_img[:,:,i], op.T) for i in range(img.shape[-1])])
@@ -59,7 +59,7 @@ def get_texture_hist(img,segment_mask,n_orientation = 8, n_bins = 10):
 	theta = np.zeros(img.shape)
 	for i in range(img.shape[-1]):theta[:,:,i] = _theta[i]
 	n_segments = len(set(segment_mask.flatten()))
-	labels = range(n_segments + 1)	
+	labels = range(n_segments + 1)
 	bins_orientation = np.linspace(-np.pi, np.pi, n_orientation + 1)
 	bins_intensity = np.linspace(0.0, 1.0, n_bins + 1)
 	bins = [labels, bins_orientation, bins_intensity]
